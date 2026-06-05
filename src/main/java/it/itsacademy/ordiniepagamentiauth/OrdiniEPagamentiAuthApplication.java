00fe886066d5cbbe9a0d019979dac6660df8dafc
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-06-05 15:00:10 +0200

    Inizialized

---
+++ src/main/java/it/itsacademy/ordiniepagamentiauth/OrdiniEPagamentiAuthApplication.java
@@ -0,0 +1,13 @@
+package it.itsacademy.ordiniepagamentiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrdiniEPagamentiAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrdiniEPagamentiAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/it/itsacademy/ordiniepagamentiauth/OrdiniEPagamentiAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package it.itsacademy.ordiniepagamentiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrdiniEPagamentiAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
