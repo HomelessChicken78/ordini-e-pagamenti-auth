531094db9d133afa59415df86b5defa80d29f6e3
HomelessChicken78 <cristiano.coccia.02003@gmail.com>
2026-06-08 12:06:57 +0200

    REFACTOR: Moved beans from /security/SecurityConfig.java to /config/SecurityConfig.java. Removed /security/SecurityConfig.java

--- src/main/java/it/itsacademy/ordiniepagamentiauth/config/SecurityConfig.java
+++ src/main/java/it/itsacademy/ordiniepagamentiauth/config/SecurityConfig.java
@@ -1,7 +1,12 @@
 package it.itsacademy.ordiniepagamentiauth.config;
 
+import it.itsacademy.ordiniepagamentiauth.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -16,6 +21,24 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(CustomUserDetailsService detailsService, PasswordEncoder encoder) {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider(detailsService);
+        authProvider.setPasswordEncoder(encoder);
+
+        return authProvider;
+    }
+
+    /**
+     * Estrae l'AuthenticationManager preconfigurato dal contesto di Spring Security
+     * e lo espone come Bean per consentirne l'iniezione all'interno dei servizi applicativi.
+     * Di default spring crea un Manager ma è privato.
+     */
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 
     @Bean
